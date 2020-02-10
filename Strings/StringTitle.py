f0d5b2ff4a07f24af4758858ee399d4c71131c31
Riya Sahal <riyasahal@yahoo.com>
2020-02-10 12:21:24 +0530

    Usage of the method Title()

---
+++ Strings/StringTitle.py
@@ -0,0 +1,11 @@
+'''
+This program demonstrates the usage of Title() method which is used to convert the first character of each word to Uppercase and remaining to Lowercase.
+Input: String(From the user)
+Returns: New String
+'''
+def title_convert( your_string ):
+    return your_string.title()
+
+your_string = raw_input("Enter a string:")
+new_string = title_convert(your_string)
+print("Your modified string: %s" % new_string)
